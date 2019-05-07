294fa038a3073b89abf9006b6893a9eae6965076
JoaoGomes2110 <joaopedro2110@hotmail.com>
2019-05-07 14:00:18 +0100

    Alterada configuração da base de dados

--- seguradoraAPP/databaseConfiguration/config.js
+++ seguradoraAPP/databaseConfiguration/config.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize')
 
-var sequelize = new Sequelize('is_grupo7', 'root', 'goleador', {
+var sequelize = new Sequelize('seguradora', 'root', 'goleador', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
