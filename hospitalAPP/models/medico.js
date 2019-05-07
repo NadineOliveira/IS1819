83d743b518249be2091642d1279023fc6fa983d1
Nadine <nadioliv@gmail.com>
2019-05-07 16:55:26 +0100

    Altera models e bds

--- hospitalAPP/models/medico.js
+++ hospitalAPP/models/medico.js
@@ -1,33 +1,38 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('medico', {
-    idMedico: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      primaryKey: true
-    },
-    nome: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    telemovel: {
-      type: DataTypes.STRING(45),
-      allowNull: false
-    },
-    area_especializacao: {
-      type: DataTypes.STRING(45),
-      allowNull: false
-    },
-    Hospital_idHospital: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      references: {
-        model: 'hospital',
-        key: 'idHospital'
+  return sequelize.define(
+    "medico",
+    {
+      idMedico: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nome: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+      },
+      telemovel: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+      },
+      area_especializacao: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+      },
+      Hospital_idHospital: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        references: {
+          model: "hospital",
+          key: "idHospital"
+        }
       }
+    },
+    {
+      tableName: "medico"
     }
-  }, {
-    tableName: 'medico'
-  });
+  );
 };
