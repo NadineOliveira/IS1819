dfbcd08d68e004e7a0666590e833c1fd23e06a85
Nadine <nadioliv@gmail.com>
2019-05-10 14:58:17 +0100

    Atualiza controllers

--- hospitalAPP/controllers/Hospital.js
+++ hospitalAPP/controllers/Hospital.js
@@ -1,10 +1,26 @@
 var Hospital = require("./associations").Hospital;
 
-module.exports.getHospitalID = async nome => {
+module.exports.getHospitalByNome = async nome => {
   var result;
   await Hospital.findOne({
     where: {
       nome: nome
+    }
+  })
+    .then(values => {
+      result = values.dataValues;
+    })
+    .catch(err => {
+      result = err;
+    });
+  return result;
+};
+
+module.exports.getHospitalByID = async id => {
+  var result;
+  await Hospital.findOne({
+    where: {
+      idHospital: id
     }
   })
     .then(values => {
